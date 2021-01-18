c661c4387d994d9b5cdb10c99d4abc9b7263de1f
madrabit <madrabot@gmail.com>
2021-01-18 16:57:20 +0300

    adding files

---
+++ src/main/java/ru/madrabit/webscrapper_spring/WebscrapperSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.madrabit.webscrapper_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebscrapperSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebscrapperSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/madrabit/webscrapper_spring/WebscrapperSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.madrabit.webscrapper_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscrapperSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
